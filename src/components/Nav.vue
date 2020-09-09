d1633604521850fa1ec55f9fe464bfc21f1a04ff
Jarrett <1206905682@qq.com>
2020-09-09 17:09:07 +0800

    解决svg自带颜色

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -28,17 +28,21 @@ nav {
   flex-direction: row;
 
   > .item {
-    padding:2px 0;
+    padding: 2px 0;
     width: 33.333333%;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    .icon{
+
+    .icon {
       width: 32px;
       height: 32px;
     }
 
+  }
+  >.item.selected{
+    color:red;
   }
 
 }

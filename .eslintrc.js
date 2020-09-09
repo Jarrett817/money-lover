7f24b72eeb0db94d5ca5d34bc971b626b0f8c473
Jarrett <1206905682@qq.com>
2020-09-09 15:52:04 +0800

    使用svg loader加载svg图片

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    //require报错
+    '@typescript-eslint/no-var-requires': 0,
   },
   overrides: [
     {
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      记账
+    </router-link>
     <router-link to="/labels">标签</router-link>
     <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-name: "Nav"
+// import x from '@/assets/icons/label.svg';
+// import y from '@/assets/icons/money.svg'
+// 过不了ts检查，需要写.d.ts
+
+
+//svg全部引入
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try{
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+
+}catch(error) {
+  console.log(error)
 }
+export default {
+  name: "Nav"
+};
 </script>
 
 <style lang="scss" scoped>
-.nav{
+.nav {
 
 }
 </style>

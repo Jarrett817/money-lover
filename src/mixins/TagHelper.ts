e45f0d49c5e39269017c578b7bc3354fa4fc75c4
Jarrett <1206905682@qq.com>
2020-10-05 16:55:27 +0800

    修改细节

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -7,7 +7,8 @@ export class TagHelper extends Vue {
     createTag() {
         const name = window.prompt("请输入标签名");
         if (!name) {
-            return window.alert("标签名不能为空");
+            console.log(name)
+            return window.alert("未输入标签名！");
         }
         this.$store.commit("createTag",name);
     }
